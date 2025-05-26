f04c0d788409cc96ae184ca12fc010645aac96cc
ASKAR <askarjon.abdullayev@gmail.com>
2025-05-26 20:35:08 +0500

    single movie page

--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from apps.movies.models import Movie
 
@@ -6,3 +6,8 @@ from apps.movies.models import Movie
 def movies_view(request):
     movies = Movie.objects.all()
     return render(request, 'movies.html', context={'movies': movies})
+
+
+def single_movie_view(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    return render(request, 'movie.html', context={'movie': movie})
