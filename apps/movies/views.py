21063f785271adeb373ff46dec8a923a02b795a4
ASKAR <askarjon.abdullayev@gmail.com>
2025-05-28 19:43:32 +0500

    feature: Search

--- apps/movies/views.py
+++ apps/movies/views.py
@@ -4,7 +4,11 @@ from apps.movies.models import Movie
 
 
 def movies_view(request):
-    movies = Movie.objects.all()
+    search = request.GET.get('search')
+    if search:
+        movies = Movie.objects.filter(title__icontains=search)
+    else:
+        movies = Movie.objects.all()
     return render(request, 'movies.html', context={'movies': movies})
 
 
